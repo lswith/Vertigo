034bc3476fdc1c33516abc77b734570e373f51a3
Luke Swithenbank <lswith@users.noreply.github.com>
2017-05-17 11:06:11 +1000

    Update TGRImageViewController.m

--- Vertigo/TGRImageViewController.m
+++ Vertigo/TGRImageViewController.m
@@ -20,6 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 #import "TGRImageViewController.h"
 
 @interface TGRImageViewController ()
