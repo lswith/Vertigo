118e765b5a9e35132868bac2508a2f046f267ed3
Luke Swithenbank <lswithenbank@freelancer.com>
2017-08-03 16:04:56 +1000

    add carthage support

---
+++ Vertigo.h
@@ -0,0 +1,19 @@
+//
+//  Vertigo.h
+//  Vertigo
+//
+//  Created by Luke Swithenbank on 3/8/17.
+//  Copyright © 2017 lswith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Vertigo.
+FOUNDATION_EXPORT double VertigoVersionNumber;
+
+//! Project version string for Vertigo.
+FOUNDATION_EXPORT const unsigned char VertigoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Vertigo/PublicHeader.h>
+
+
